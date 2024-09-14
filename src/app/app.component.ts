c427cb2de8203ee081d8aeffc75d1ed861cd496e
Diego Gutierrez <diegofgutibol@gmail.com>
2024-09-14 16:19:22 -0500

    Adding route functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from "./home/home.component";
-
+import { RouterModule } from '@angular/router';
 @Component({
   standalone: true,
   selector: 'app-root',
@@ -11,11 +11,11 @@ import { HomeComponent } from "./home/home.component";
     </header>
   </main>
   <section>
-    <app-home />
+    <router-outlet></router-outlet>
   </section>
   `,
   styleUrls: ['./app.component.css'],
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
 })
 export class AppComponent {
   title = 'homes';
